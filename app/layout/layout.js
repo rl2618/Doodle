93b1de898f7d93543652cbf2869365f1705fa996
rl2618 <rl2618@columbia.edu>
2017-02-20 20:48:36 -0500

    fixed data binding

--- app/layout/layout.js
+++ app/layout/layout.js
@@ -1,18 +1,10 @@
 /**
  * Created by Abraham on 2/18/2017.
  */
-(function(){
-    'use strict';
 
-    angular
-        .module('app.layout')
-        .controller('Layout', Layout);
 
-    function Layout() {
-        var vm = this;
-
-        vm.title = 'Things I Made Viewing Page';
-        vm.subtitle = 'Welcome to the 3D Modeling Page!';
-
-    }
-})();
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+    $scope.title = 'Things I Made Viewing Page';
+    $scope.subtitle = 'Welcome to the 3D Modeling Page';
+});
